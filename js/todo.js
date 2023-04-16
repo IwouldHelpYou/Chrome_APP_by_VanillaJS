ec0027b556b5bf7febe893db65334395f63de7e9
IwouldHelpYou <dbf6609@naver.com>
2023-04-16 19:54:06 +0900

    23.4.1

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,8 @@
-<form id="todo-form">
-    <input type="text" placeholder="써써" required/>
-</form>
+<!DOCTYPE html>
+<html>
+    <body>
+        <form id="todo-form">
+            <input type="text" placeholder="써써" required/>
+        </form>
+    </body>
+</html>
